379a66fe3fe49a8d3895e86eb3269f8dc7821afb
Thomas <thomas.lachieze@gmail.com>
2020-03-31 18:37:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import QuoteCard from './components/QuoteCard';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sampleCharacter = 
+  {
+  "quote": "By chilling my loins I increase the chances of impregnating my wife.",
+  "character": "Apu Nahasapeemapetilon",
+  "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+  "characterDirection": "Left"
+  };
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      character: sampleCharacter
+    };
+    this.getCharacter = this.getCharacter.bind(this);
+  }
+  getCharacter() {
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then(response => response.data)
+      .then(data => {
+        console.log(data);
+        this.setState({
+          character: data[0],
+        });
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <QuoteCard character={this.state.character} />
+        <button type="button" onClick={this.getCharacter}>Get character</button>
+      </div>
+    );
+  }
 }
 
 export default App;
